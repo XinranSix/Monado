6fefa34793df6db76f104da839bd498d08af043a
Yao J <jieyao.lxr@foxmail.com>
2024-03-16 23:33:14 +0800

    Serializing script fields

--- editor/editorLayer.cpp
+++ editor/editorLayer.cpp
@@ -53,8 +53,7 @@ namespace Monado {
         auto commandLineArgs = Application::Get().GetSpecification().CommandLineArgs;
         if (commandLineArgs.Count > 1) {
             auto sceneFilePath = commandLineArgs[1];
-            SceneSerializer serializer(m_ActiveScene);
-            serializer.Deserialize(sceneFilePath);
+            OpenScene(sceneFilePath);
         }
 
         m_EditorCamera = EditorCamera(30.0f, 1.778f, 0.1f, 1000.0f);
@@ -237,7 +236,7 @@ namespace Monado {
 
         m_ViewportFocused = ImGui::IsWindowFocused();
         m_ViewportHovered = ImGui::IsWindowHovered();
-        Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused && !m_ViewportHovered);
+        Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportHovered);
 
         ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
         m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
--- src/monado/scene/sceneSerializer.cpp
+++ src/monado/scene/sceneSerializer.cpp
@@ -5,6 +5,9 @@
 #include "monado/scene/sceneSerializer.h"
 #include "monado/scene/entity.h"
 #include "monado/scene/components.h"
+#include "monado/scripting/scriptEngine.h"
+#include "monado/core/uuid.h"
+
 
 namespace YAML {
 
@@ -74,8 +77,33 @@ namespace YAML {
         }
     };
 
+    template <>
+    struct convert<Monado::UUID> {
+        static Node encode(const Monado::UUID &uuid) {
+            Node node;
+            node.push_back((uint64_t)uuid);
+            return node;
+        }
+
+        static bool decode(const Node &node, Monado::UUID &uuid) {
+            uuid = node.as<uint64_t>();
+            return true;
+        }
+    };
+
 } // namespace YAML
+
 namespace Monado {
+
+#define WRITE_SCRIPT_FIELD(FieldType, Type)                                                                            \
+    case ScriptFieldType::FieldType: out << scriptField.GetValue<Type>(); break
+
+#define READ_SCRIPT_FIELD(FieldType, Type)                                                                             \
+    case ScriptFieldType::FieldType: {                                                                                 \
+        Type data = scriptField["Data"].as<Type>();                                                                    \
+        fieldInstance.SetValue(data);                                                                                  \
+        break;                                                                                                         \
+    }
 
     YAML::Emitter &operator<<(YAML::Emitter &out, const glm::vec2 &v) {
         out << YAML::Flow;
@@ -178,6 +206,48 @@ namespace Monado {
             out << YAML::Key << "ScriptComponent";
             out << YAML::BeginMap; // ScriptComponent
             out << YAML::Key << "ClassName" << YAML::Value << scriptComponent.ClassName;
+
+            // Fields
+            Ref<ScriptClass> entityClass = ScriptEngine::GetEntityClass(scriptComponent.ClassName);
+            const auto &fields = entityClass->GetFields();
+            if (fields.size() > 0) {
+                out << YAML::Key << "ScriptFields" << YAML::Value;
+                auto &entityFields = ScriptEngine::GetScriptFieldMap(entity);
+                out << YAML::BeginSeq;
+                for (const auto &[name, field] : fields) {
+                    if (entityFields.find(name) == entityFields.end())
+                        continue;
+
+                    out << YAML::BeginMap; // ScriptField
+                    out << YAML::Key << "Name" << YAML::Value << name;
+                    out << YAML::Key << "Type" << YAML::Value << Utils::ScriptFieldTypeToString(field.Type);
+
+                    out << YAML::Key << "Data" << YAML::Value;
+                    ScriptFieldInstance &scriptField = entityFields.at(name);
+
+                    switch (field.Type) {
+                        WRITE_SCRIPT_FIELD(Float, float);
+                        WRITE_SCRIPT_FIELD(Double, double);
+                        WRITE_SCRIPT_FIELD(Bool, bool);
+                        WRITE_SCRIPT_FIELD(Char, char);
+                        WRITE_SCRIPT_FIELD(Byte, int8_t);
+                        WRITE_SCRIPT_FIELD(Short, int16_t);
+                        WRITE_SCRIPT_FIELD(Int, int32_t);
+                        WRITE_SCRIPT_FIELD(Long, int64_t);
+                        WRITE_SCRIPT_FIELD(UByte, uint8_t);
+                        WRITE_SCRIPT_FIELD(UShort, uint16_t);
+                        WRITE_SCRIPT_FIELD(UInt, uint32_t);
+                        WRITE_SCRIPT_FIELD(ULong, uint64_t);
+                        WRITE_SCRIPT_FIELD(Vector2, glm::vec2);
+                        WRITE_SCRIPT_FIELD(Vector3, glm::vec3);
+                        WRITE_SCRIPT_FIELD(Vector4, glm::vec4);
+                        WRITE_SCRIPT_FIELD(Entity, UUID);
+                    }
+                    out << YAML::EndMap; // ScriptFields
+                }
+                out << YAML::EndSeq;
+            }
+
             out << YAML::EndMap; // ScriptComponent
         }
 
@@ -280,7 +350,7 @@ namespace Monado {
         try {
             data = YAML::LoadFile(filepath);
         } catch (YAML::ParserException e) {
-            MND_CORE_ERROR("Failed to load .hazel file '{0}'\n     {1}", filepath, e.what());
+            MND_CORE_ERROR("Failed to load .Monado file '{0}'\n     {1}", filepath, e.what());
             return false;
         }
 
@@ -336,6 +406,49 @@ namespace Monado {
                 if (scriptComponent) {
                     auto &sc = deserializedEntity.AddComponent<ScriptComponent>();
                     sc.ClassName = scriptComponent["ClassName"].as<std::string>();
+
+                    auto scriptFields = scriptComponent["ScriptFields"];
+                    if (scriptFields) {
+                        Ref<ScriptClass> entityClass = ScriptEngine::GetEntityClass(sc.ClassName);
+                        MND_CORE_ASSERT(entityClass);
+                        const auto &fields = entityClass->GetFields();
+                        auto &entityFields = ScriptEngine::GetScriptFieldMap(deserializedEntity);
+
+                        for (auto scriptField : scriptFields) {
+                            std::string name = scriptField["Name"].as<std::string>();
+                            std::string typeString = scriptField["Type"].as<std::string>();
+                            ScriptFieldType type = Utils::ScriptFieldTypeFromString(typeString);
+
+                            ScriptFieldInstance &fieldInstance = entityFields[name];
+
+                            // TODO(Yan): turn this assert into Monadonut log warning
+                            MND_CORE_ASSERT(fields.find(name) != fields.end());
+
+                            if (fields.find(name) == fields.end())
+                                continue;
+
+                            fieldInstance.Field = fields.at(name);
+
+                            switch (type) {
+                                READ_SCRIPT_FIELD(Float, float);
+                                READ_SCRIPT_FIELD(Double, double);
+                                READ_SCRIPT_FIELD(Bool, bool);
+                                READ_SCRIPT_FIELD(Char, char);
+                                READ_SCRIPT_FIELD(Byte, int8_t);
+                                READ_SCRIPT_FIELD(Short, int16_t);
+                                READ_SCRIPT_FIELD(Int, int32_t);
+                                READ_SCRIPT_FIELD(Long, int64_t);
+                                READ_SCRIPT_FIELD(UByte, uint8_t);
+                                READ_SCRIPT_FIELD(UShort, uint16_t);
+                                READ_SCRIPT_FIELD(UInt, uint32_t);
+                                READ_SCRIPT_FIELD(ULong, uint64_t);
+                                READ_SCRIPT_FIELD(Vector2, glm::vec2);
+                                READ_SCRIPT_FIELD(Vector3, glm::vec3);
+                                READ_SCRIPT_FIELD(Vector4, glm::vec4);
+                                READ_SCRIPT_FIELD(Entity, UUID);
+                            }
+                        }
+                    }
                 }
 
                 auto spriteRendererComponent = entity["SpriteRendererComponent"];
