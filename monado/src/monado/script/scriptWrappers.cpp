2a36a184c06b7a9fa64cfe401aa7d17f170de3ae
Yao J <jieyao.lxr@foxmail.com>
2024-03-24 16:31:57 +0800

    Added support for MeshColliders when calling Overlap functions

--- monado/src/monado/script/scriptWrappers.cpp
+++ monado/src/monado/script/scriptWrappers.cpp
@@ -73,79 +73,64 @@ namespace Monado {
             return PXPhysicsWrappers::Raycast(*origin, *direction, maxDistance, hit);
         }
 
+        static void AddCollidersToArray(MonoArray *array, const std::vector<physx::PxOverlapHit> &hits) {
+            uint32_t arrayIndex = 0;
+            for (const auto &hit : hits) {
+                Entity &entity = *(Entity *)hit.actor->userData;
+
+                if (entity.HasComponent<BoxColliderComponent>()) {
+                    auto &boxCollider = entity.GetComponent<BoxColliderComponent>();
+
+                    void *data[] = { &entity.GetUUID(), &boxCollider.IsTrigger, &boxCollider.Size,
+                                     &boxCollider.Offset };
+
+                    MonoObject *obj =
+                        ScriptEngine::Construct("Monado.BoxCollider:.ctor(ulong,bool,Vector3,Vector3)", true, data);
+                    mono_array_set(array, MonoObject *, arrayIndex++, obj);
+                }
+
+                if (entity.HasComponent<SphereColliderComponent>()) {
+                    auto &sphereCollider = entity.GetComponent<SphereColliderComponent>();
+
+                    void *data[] = { &entity.GetUUID(), &sphereCollider.IsTrigger, &sphereCollider.Radius };
+
+                    MonoObject *obj =
+                        ScriptEngine::Construct("Monado.SphereCollider:.ctor(ulong,bool,float)", true, data);
+                    mono_array_set(array, MonoObject *, arrayIndex++, obj);
+                }
+
+                if (entity.HasComponent<CapsuleColliderComponent>()) {
+                    auto &capsuleCollider = entity.GetComponent<CapsuleColliderComponent>();
+
+                    void *data[] = { &entity.GetUUID(), &capsuleCollider.IsTrigger, &capsuleCollider.Radius,
+                                     &capsuleCollider.Height };
+
+                    MonoObject *obj =
+                        ScriptEngine::Construct("Monado.CapsuleCollider:.ctor(ulong,bool,float,float)", true, data);
+                    mono_array_set(array, MonoObject *, arrayIndex++, obj);
+                }
+
+                if (entity.HasComponent<MeshColliderComponent>()) {
+                    auto &meshCollider = entity.GetComponent<MeshColliderComponent>();
+
+                    Ref<Mesh> *mesh = new Ref<Mesh>(meshCollider.CollisionMesh);
+                    void *data[] = { &entity.GetUUID(), &meshCollider.IsTrigger, &mesh };
+
+                    MonoObject *obj =
+                        ScriptEngine::Construct("Monado.MeshCollider:.ctor(ulong,bool,intptr)", true, data);
+                    mono_array_set(array, MonoObject *, arrayIndex++, obj);
+                }
+            }
+        }
+
         MonoArray *Monado_Physics_OverlapBox(glm::vec3 *origin, glm::vec3 *halfSize) {
             MonoArray *outColliders = nullptr;
-            uint32_t arrayIndex = 0;
             std::vector<physx::PxOverlapHit> buffer;
+
             if (PXPhysicsWrappers::OverlapBox(*origin, *halfSize, buffer)) {
-                // TODO: Exclude MeshColliders for now
-                uintptr_t count = buffer.size();
-                for (const auto &hit : buffer) {
-                    Entity &entity = *(Entity *)hit.actor->userData;
-
-                    if (!entity.HasComponent<BoxColliderComponent>() &&
-                        !entity.HasComponent<SphereColliderComponent>() &&
-                        !entity.HasComponent<CapsuleColliderComponent>()) {
-                        if (entity.HasComponent<MeshColliderComponent>()) {
-                            count--;
-                        }
-                    }
-                }
-
-                outColliders = mono_array_new(mono_domain_get(), ScriptEngine::GetCoreClass("Monado.Collider"), count);
-
-                for (const auto &hit : buffer) {
-                    if (arrayIndex >= count)
-                        break;
-
-                    Entity &entity = *(Entity *)hit.actor->userData;
-
-                    if (entity.HasComponent<BoxColliderComponent>()) {
-                        auto &boxCollider = entity.GetComponent<BoxColliderComponent>();
-
-                        void *data[] = { &entity.GetUUID(), &boxCollider.IsTrigger, &boxCollider.Size,
-                                         &boxCollider.Offset };
-
-                        MonoObject *obj =
-                            ScriptEngine::Construct("Monado.BoxCollider:.ctor(ulong,bool,Vector3,Vector3)", true, data);
-                        mono_array_set(outColliders, MonoObject *, arrayIndex++, obj);
-                    }
-
-                    if (entity.HasComponent<SphereColliderComponent>()) {
-                        auto &sphereCollider = entity.GetComponent<SphereColliderComponent>();
-
-                        void *data[] = { &entity.GetUUID(), &sphereCollider.IsTrigger, &sphereCollider.Radius };
-
-                        MonoObject *obj =
-                            ScriptEngine::Construct("Monado.SphereCollider:.ctor(ulong,bool,float)", true, data);
-                        mono_array_set(outColliders, MonoObject *, arrayIndex++, obj);
-                    }
-
-                    if (entity.HasComponent<CapsuleColliderComponent>()) {
-                        auto &capsuleCollider = entity.GetComponent<CapsuleColliderComponent>();
-
-                        void *data[] = { &entity.GetUUID(), &capsuleCollider.IsTrigger, &capsuleCollider.Radius,
-                                         &capsuleCollider.Height };
-
-                        MonoObject *obj =
-                            ScriptEngine::Construct("Monado.CapsuleCollider:.ctor(ulong,bool,float,float)", true, data);
-                        mono_array_set(outColliders, MonoObject *, arrayIndex++, obj);
-                    }
-
-                    /*if (entity.HasComponent<MeshColliderComponent>())
-                    {
-                            auto& meshCollider = entity.GetComponent<MeshColliderComponent>();
-
-                            MonoString* filepath = mono_string_new(mono_domain_get(),
-                    meshCollider.CollisionMesh->GetFilePath().c_str()); void* data[] = { &entity.GetUUID(),
-                                    &meshCollider.IsTrigger,
-                                    &filepath
-                            };
-
-                            MonoObject* obj = ScriptEngine::Construct("Monado.MeshCollider:.ctor(ulong,bool,string)",
-                    true, data); mono_array_set(outColliders, MonoObject*, arrayIndex++, obj);
-                    }*/
-                }
+                outColliders =
+                    mono_array_new(mono_domain_get(), ScriptEngine::GetCoreClass("Monado.Collider"), buffer.size());
+                AddCollidersToArray(outColliders, buffer);
             }
 
             return outColliders;
@@ -153,77 +138,12 @@ namespace Monado {
 
         MonoArray *Monado_Physics_OverlapSphere(glm::vec3 *origin, float radius) {
             MonoArray *outColliders = nullptr;
-            uint32_t arrayIndex = 0;
             std::vector<physx::PxOverlapHit> buffer;
+
             if (PXPhysicsWrappers::OverlapSphere(*origin, radius, buffer)) {
-                // TODO: Exclude MeshColliders for now
-                uintptr_t count = buffer.size();
-                for (const auto &hit : buffer) {
-                    Entity &entity = *(Entity *)hit.actor->userData;
-
-                    if (!entity.HasComponent<BoxColliderComponent>() &&
-                        !entity.HasComponent<SphereColliderComponent>() &&
-                        !entity.HasComponent<CapsuleColliderComponent>()) {
-                        if (entity.HasComponent<MeshColliderComponent>()) {
-                            count--;
-                        }
-                    }
-                }
-
-                outColliders = mono_array_new(mono_domain_get(), ScriptEngine::GetCoreClass("Monado.Collider"), count);
-
-                for (const auto &hit : buffer) {
-                    if (arrayIndex >= count)
-                        break;
-
-                    Entity &entity = *(Entity *)hit.actor->userData;
-
-                    if (entity.HasComponent<BoxColliderComponent>()) {
-                        auto &boxCollider = entity.GetComponent<BoxColliderComponent>();
-
-                        void *data[] = { &entity.GetUUID(), &boxCollider.IsTrigger, &boxCollider.Size,
-                                         &boxCollider.Offset };
-
-                        MonoObject *obj =
-                            ScriptEngine::Construct("Monado.BoxCollider:.ctor(ulong,bool,Vector3,Vector3)", true, data);
-                        mono_array_set(outColliders, MonoObject *, arrayIndex++, obj);
-                    }
-
-                    if (entity.HasComponent<SphereColliderComponent>()) {
-                        auto &sphereCollider = entity.GetComponent<SphereColliderComponent>();
-
-                        void *data[] = { &entity.GetUUID(), &sphereCollider.IsTrigger, &sphereCollider.Radius };
-
-                        MonoObject *obj =
-                            ScriptEngine::Construct("Monado.SphereCollider:.ctor(ulong,bool,float)", true, data);
-                        mono_array_set(outColliders, MonoObject *, arrayIndex++, obj);
-                    }
-
-                    if (entity.HasComponent<CapsuleColliderComponent>()) {
-                        auto &capsuleCollider = entity.GetComponent<CapsuleColliderComponent>();
-
-                        void *data[] = { &entity.GetUUID(), &capsuleCollider.IsTrigger, &capsuleCollider.Radius,
-                                         &capsuleCollider.Height };
-
-                        MonoObject *obj =
-                            ScriptEngine::Construct("Monado.CapsuleCollider:.ctor(ulong,bool,float,float)", true, data);
-                        mono_array_set(outColliders, MonoObject *, arrayIndex++, obj);
-                    }
-
-                    /*if (entity.HasComponent<MeshColliderComponent>())
-                    {
-                            auto& meshCollider = entity.GetComponent<MeshColliderComponent>();
-
-                            MonoString* filepath = mono_string_new(mono_domain_get(),
-                    meshCollider.CollisionMesh->GetFilePath().c_str()); void* data[] = { &entity.GetUUID(),
-                                    &meshCollider.IsTrigger,
-                                    &filepath
-                            };
-
-                            MonoObject* obj = ScriptEngine::Construct("Monado.MeshCollider:.ctor(ulong,bool,string)",
-                    true, data); mono_array_set(outColliders, MonoObject*, arrayIndex++, obj);
-                    }*/
-                }
+                outColliders =
+                    mono_array_new(mono_domain_get(), ScriptEngine::GetCoreClass("Monado.Collider"), buffer.size());
+                AddCollidersToArray(outColliders, buffer);
             }
 
             return outColliders;
--- mondoScriptCore/src/monado/physics/Collider.cs
+++ mondoScriptCore/src/monado/physics/Collider.cs
@@ -2,37 +2,11 @@
 
 namespace Monado
 {
-	public enum ColliderType
-    {
-		Box = 0,
-		Sphere = 1,
-		Capsule = 2
-    }
-
 	public class Collider
 	{
-		public ColliderType Type { get; protected set; }
 		public ulong EntityID { get; protected set; }
 		public bool IsTrigger { get; protected set; }
-
-		public bool Is<T>() where T : Collider
-        {
-			if (typeof(T) == typeof(BoxCollider) && Type == ColliderType.Box)
-            {
-				return true;
-            }
-			else if (typeof(T) == typeof(SphereCollider) && Type == ColliderType.Sphere)
-            {
-                return true;
-            }
-			else if (typeof(T) == typeof(CapsuleCollider) && Type == ColliderType.Capsule)
-            {
-                return true;
-            }
-
-			return false;
-        }
-    }
+	}
 
 	public class BoxCollider : Collider
 	{
@@ -40,52 +14,49 @@ namespace Monado
 		public Vector3 Offset { get; protected set; }
 
 		private BoxCollider(ulong entityID, bool isTrigger, Vector3 size, Vector3 offset)
-        {
-			Type = ColliderType.Box;
+		{
 			EntityID = entityID;
 			IsTrigger = isTrigger;
 			Size = size;
 			Offset = offset;
-        }
+		}
 	}
 
 	public class SphereCollider : Collider
 	{
-        public float Radius { get; protected set; }
+		public float Radius { get; protected set; }
 
-        private SphereCollider(ulong entityID, bool isTrigger, float radius)
-        {
-            Type = ColliderType.Sphere;
-            EntityID = entityID;
-            IsTrigger = isTrigger;
+		private SphereCollider(ulong entityID, bool isTrigger, float radius)
+		{
+			EntityID = entityID;
+			IsTrigger = isTrigger;
 			Radius = radius;
-        }
-    }
+		}
+	}
 
 	public class CapsuleCollider : Collider
 	{
-        public float Radius { get; protected set; }
+		public float Radius { get; protected set; }
 		public float Height { get; protected set; }
 
-        private CapsuleCollider(ulong entityID, bool isTrigger, float radius, float height)
-        {
-            Type = ColliderType.Capsule;
-            EntityID = entityID;
-            IsTrigger = isTrigger;
-            Radius = radius;
+		private CapsuleCollider(ulong entityID, bool isTrigger, float radius, float height)
+		{
+			EntityID = entityID;
+			IsTrigger = isTrigger;
+			Radius = radius;
 			Height = height;
-        }
-    }
+		}
+	}
 
 	public class MeshCollider : Collider
 	{
-        public Mesh Mesh { get; protected set; }
+		public Mesh Mesh { get; protected set; }
 
-		private MeshCollider(ulong entityID, bool isTrigger, string filepath)
-        {
+		private MeshCollider(ulong entityID, bool isTrigger, IntPtr mesh)
+		{
 			EntityID = entityID;
 			IsTrigger = isTrigger;
-			Mesh = new Mesh(filepath);
-        }
+			Mesh = new Mesh(mesh);
+		}
 	}
 }
